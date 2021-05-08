77a0d7c530145b52712c6ae2d93316b8963abd36
willembr <willembrants@hotmail.com>
2021-05-08 09:46:13 +0200

    new checkout

--- src/Components/customers/checkout/checkout.js
+++ src/Components/customers/checkout/checkout.js
@@ -4,12 +4,12 @@ import CocktailImg from '../../../assets/images/cocktail.png';
 
 const CheckOut = (props) => (
     <div className="CheckOut" >
-            <p>Deze gegevens zullen na 4 weken verwijderd worden!</p>
+            {/* <p>Deze gegevens zullen na 4 weken verwijderd worden!</p> */}
             <p>Geniet van onze cocktails, wijntjes en bites!</p>
             <img src={CocktailImg} alt="coctail" />
-            <h2>Coctail van de week</h2>
+            {/* <h2>Coctail van de week</h2>
             <p>Dark 'n Stormy</p>
-            <p>Havana 5y, ginger beer and lime cocktail</p>
+            <p>Havana 5y, ginger beer and lime cocktail</p> */}
             </div>
 );
 
